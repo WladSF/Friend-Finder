db5f8169c85affc31a96d17cbb483197bd86b531
WladSF <wpaivasf@gmail.com>
2019-01-02 17:52:07 -0800

    Updated files

--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -1,8 +1,6 @@
 //Load data from main database friends
 //============================================
 var friendsName = require("../Data/friends");
-var newArray = [];
-
 
 //Route
 //============================================
@@ -13,8 +11,39 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        for (var i = 0; i < req.body.scores.length; i++) {
-            newArray.push(req.body.scores[i]);
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        var userInfo = req.body;
+        var userScores = userInfo.scores;
+        var totalDifference;
+
+        for (var i = 0; i < friendsName.length; i++) {
+            var currentFriend = friendsName[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+
         }
+        friends.push(userInfo);
+
+        res.json(bestMatch);
+
     });
 };
